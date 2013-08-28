7a00d43597ed27c26a327b768619e53cce23df90
Trenton Large <trentonlarge@gmail.com>
2013-08-28 11:56:15 -0400

    Added debug prompt

--- Simulatron/simulatron.c
+++ Simulatron/simulatron.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print(int * array, int length);
+void print(short instruction, int accumulator, int * memory, short mem_length);
 
 int main(int argc, char * argv[]) {
 	if(argc < 2 || argc > 3) {
@@ -54,8 +54,6 @@ int main(int argc, char * argv[]) {
 	do {
 		if(i >= mem_length) {
 			fprintf(stderr, "Error: Not enough memory to load program\n");
-			if(debug)
-				print(memory, mem_length);
 			free(memory);
 			fclose(program);
 			return 3;
@@ -68,17 +66,51 @@ int main(int argc, char * argv[]) {
 
 	fclose(program);
 
+	short error = 0;
+	short debug_prompt = debug;
+	short breakpoint = -1;
 	while(1) {
+		while(debug_prompt || error || instruction == breakpoint) {
+			char command;
+			char args[10];
+
+			printf("(dgb) ");
+			short num_args = scanf("%1s %10s", &command, args);
+			if(num_args == 0)
+				continue;
+
+			if(command == 'r')
+				debug_prompt = 0;
+			else if(command == 'p')
+				print(instruction, accumulator, memory, mem_length);
+			else if(command == 's')
+				break;
+			else if(command == 'b') {
+				if(!sscanf(args, "%hd", &breakpoint))
+					printf("Usage: b <instruction>\n");
+			}
+			else if(command == 'q')
+				return 0;
+			else if(command == 'h')
+				printf("r - Run program from beginning or continue from current position.\np - Print the instruction pointer, instruction register, accumulator, and memory\ns - Step one instruction\nb - Set a breakpoint\nq - Quit\nh - Display this help\n");
+			else
+				printf("Unknown command");
+		}
+
+		if(error) {
+			free(memory);
+			return error;
+		}
+
 		if(instruction >= mem_length) {
 			fprintf(stderr, "Error: Hit end of memory space\n");
-			if(debug)
-				print(memory, mem_length);
+			if(debug) {
+				error = 5;
+				continue;
+			}
 			free(memory);
 			return 5;
 		}
-
-		if(debug)
-			fprintf(stderr, "Accumulator: %d\nInstruction Pointer: %d\nInstruction Register: %d\n", accumulator, instruction, memory[instruction]);
 
 		if(memory[instruction] == 0) {
 			if(warnings)
@@ -87,8 +119,10 @@ int main(int argc, char * argv[]) {
 				instruction++;
 				if(instruction >= mem_length) {
 					fprintf(stderr, "Error: Hit end of memory space\n");
-					if(debug)
-						print(memory, mem_length);
+					if(debug) {
+						error = 5;
+						continue;
+					}
 					free(memory);
 					return 5;
 				}
@@ -100,8 +134,10 @@ int main(int argc, char * argv[]) {
 
 		if(argument < 0 || argument >= mem_length) {
 			fprintf(stderr, "Error: Invalid argument %d at %d\n", argument, instruction);
-			if(debug)
-				print(memory, mem_length);
+			if(debug) {
+				error = 4;
+				continue;
+			}
 			free(memory);
 			return 4;
 		}
@@ -111,8 +147,10 @@ int main(int argc, char * argv[]) {
 				printf("> ");
 				if(!scanf("%d", &(memory[argument]))) {
 					fprintf(stderr, "Error: Input not a number\n");
-					if(debug)
-						print(memory, mem_length);
+					if(debug) {
+						error = 6;
+						continue;
+					}
 					free(memory);
 					return 6;
 				}
@@ -141,8 +179,10 @@ int main(int argc, char * argv[]) {
 			case 32:
 				if(memory[argument] == 0) {
 					fprintf(stderr, "Error: Division by zero at %d\n", instruction);
-					if(debug)
-						print(memory, mem_length);
+					if(debug) {
+						error = 4;
+						continue;
+					}
 					free(memory);
 					return 4;
 				}
@@ -167,21 +207,16 @@ int main(int argc, char * argv[]) {
 					instruction = argument;
 				break;
 			case 43:
-				if(debug) {
-					printf("Would you like to print the memory? [N/y] ");
-					char response[3];
-					int result = scanf("%3s", response);
-					if(result && (strcmp(response, "y") == 0 || strcmp(response, "yes") == 0))
-						print(memory, mem_length);
-				}
 				free(memory);
 				return 0;
 			case 50:
 				break;
 			default:
 				fprintf(stderr, "Error: Unknown opcode %d at %d\n", opcode, instruction);
-				if(debug)
-					print(memory, mem_length);
+				if(debug) {
+					error = 4;
+					continue;
+				}
 				free(memory);
 				return 4;
 		}
@@ -190,13 +225,18 @@ int main(int argc, char * argv[]) {
 	}
 }
 
-void print(int * array, int length) {
-	int number_width = ceil(log10(length));
+void print(short instruction, int accumulator, int * memory, short mem_length) {
+	fprintf(stderr, "Instruction Pointer: %hd\n", instruction);
+	fprintf(stderr, "Instruction Register: %d\n", memory[instruction]);
+
+	fprintf(stderr, "Accumulator: %d\n", accumulator);
+
+	int number_width = ceil(log10(mem_length));
 	fprintf(stderr, "Memory:\n");
-	for(int i = 0; i < length; i++) {
+	for(int i = 0; i < mem_length; i++) {
 		fprintf(stderr, "%*d |", i, number_width);
 		for(int ii = 0; ii < 10; ii++, i++)
-			fprintf(stderr, " %d", array[i]);
+			fprintf(stderr, " %d", memory[i]);
 		fprintf(stderr, "\n");
 	}
 }
