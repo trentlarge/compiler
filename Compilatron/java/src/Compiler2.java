6843623f730d31229357dc0ec1eb336bcba2898d
J-Lowe <jonlowe4459@gmail.com>
2013-09-06 11:55:08 -0400

    Changed file names

--- Compilatron/java/src/Compiler2.java
+++ Compilatron/java/src/Compiler2.java
@@ -1,94 +1,111 @@
+//package tk.jlowe.smla.compiler;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Compiler {
-	static Pattern relation_pattern = Pattern.compile("(.+)(>|<|>=|<=|==|!=)+(.+)");
-	static Pattern expression_pattern = Pattern.compile("([a-zA-Z0-9+-*/()]+)");
+public class Compiler2 {
+	private HashMap<Symbol, Integer> symbolTable;
+	private HashMap<String, Integer> commandTable;
+	private final String[] usableCommands = {"rem", "input", "print", "let", "goto", "if", "end"};
+	private int commandPointer = 0;
+	private int memoryPointer = 99;
+	private String machineCodeContents;
 
-	Scanner scanner;
-	int[] memory;
-	ArrayList<Integer> constants;
-	HashMap<Integer, Integer> line_numbers;
-	HashMap<String, Integer> variables;
-	int line_number, pointer, data_pointer;
-
-	public Compiler(File file) {
-		scanner = new Scanner(file);
-		memory = new int[100];
-		constants = new ArrayList<Integer>();
-		line_numbers = new HashMap<Integer, Integer>();
-		variables = new HashMap<String, Integer>();
-		line_number = 0;
-		pointer = 0;
-		data_pointer = 99;
+	public Compiler2(File file) {
+		symbolTable = new HashMap<Symbol, Integer>();
+		machineCodeContents = "";
+		commandTable.put(usableCommands[1], 10);
+		commandTable.put(usableCommands[2], 11);
+		commandTable.put(usableCommands[6], 43);
 	}
 
-	public int[] compile() {
-		while(scanner.hasNextLine()) {
-			if(pointer >= data_pointer)
-				throw new OutOfMemoryException();
-
-			String[] command = scanner.nextLine().split(" ");
-
-			int next_line_number = Integer.parseInt(command[0]);
-			if(next_line_number <= line_number)
-				throw new LineNumberException();
-			line_numbers.put(next_line_number, pointer);
-			line_number = next_line_number;
-
-			if(command[1].equalsIgnoreCase("rem")) //Ingore comment lines
-				continue;
-			else if(command[1].equalsIgnoreCase("input")) { //Make a new variable and remember it
-				if(!Character.isLetter(command[2].charAt(0)))
-					throw new InvalidVariableException();
-
-				variables.put(command[2], data_pointer);
-				memory[pointer] = 1000 + data_pointer;
-				data_pointer--;
+	public void compileFile(File file) throws IOException {
+		Scanner reader = new Scanner(file);
+		String[][] lines = new String[99][];
+		int linePlace = 0;
+		while(reader.hasNextLine()) {
+			String line = reader.nextLine();
+			String[] lineComponents = line.split(" ");
+			String lineNumber = "";
+			String command = "";
+			String parameter = "";
+			try {
+				int number = Integer.parseInt(lineComponents[0]);
+				lineNumber = lineComponents[0];
+			} catch(NumberFormatException e) {
+				//TODO what to do if a line number isn't the first thing
 			}
-			else if(command[1].equalsIgnoreCase("print")) { //Simply print variable
-				if(!variables.containsKey(command[2]))
-					throw new UndefinedVariableException();
-
-				memory[pointer] = 1100 + variables.get(command[2]);
-			}
-			else if(command[1].equalsIgnoreCase("let")) { //If a variable doesn't exist, create it then parse the expression
-				if(!variables.containsKey(command[2])) {
-					variables.put(command[2], data_pointer);
-					data_pointer--;
+			command = lineComponents[1];
+			if(command.equals(usableCommands[0]) 
+					|| command.equals(usableCommands[1]) 
+					|| command.equals(usableCommands[2]) 
+					//|| command.equals(usableCommands[3]) 
+					//|| command.equals(usableCommands[4]) 
+					//|| command.equals(usableCommands[5]) 
+					|| command.equals(usableCommands[6])
+			  ) {
+				int ii = 2;
+				if(ii < lineComponents.length) {
+					parameter += lineComponents[ii] + " ";
 				}
-
-				parseExpression(command[3], variables.get(command[2]));
-			}
-			else if(command[1].equalsIgnoreCase("goto")) { //Put a new goto
-				memory[pointer] = 4000 + Integer.parseInt(command[2]);
-			}
-			else if(command[1].equalsIgnoreCase("if")) { //Yay for if's
-				if(!command[3].equalsIgnoreCase("goto"))
-					throw new IllegalArgumentException();
-				parseRelation(command[2]);//Call parse relation
-				memory[pointer] = 4100 + Integer.parseInt(command[4]);//Branch 0 to "goto"
-			}
-			else if(command[1].equalsIgnoreCase("end")) //Put a halt
-				memory[pointer] = 4300;
-
-			pointer++;
+			} else { //Not a valid command
+				System.out.println("Error: Invalid command on line " + 0); //TODO Replace 0 with line number variable
+				System.exit(0);
+			} 
+			String[] code = {command, lineNumber, parameter};
+			lines[linePlace] = code;
+			linePlace++;
 		}
+
+		compileCode(lines);
+		String path = file.getParent();
+		PrintWriter writer = new PrintWriter(new FileWriter(path + file.getName() + ".jsc"));
+		writer.println(machineCodeContents);
+		writer.close();
 	}
 
-	private void parseRelation(String relation) {
-		Matcher matcher = relation_pattern.matcher(relation);
-		if(!matcher.matches())
-			throw new SyntaxException();
-	}
-
-	private void parseExpression(String expression, int value_pointer) {
-		Matcher matcher = expression_pattern.matcher(expression);
-		if(!matcher.matches())
-			throw new SyntaxException();
+	private void compileCode(String[][] code) {
+		//Object[][] newCode = new Object[code.length][3];
+		ArrayList<Object[]> temp = new ArrayList<Object[]>();
+		for(String[] line : code) {
+			//ArrayList<Symbol> newLine = new ArrayList<Symbol>();
+			Object[] objectList = new Object[3];
+			if(line[0].equals(usableCommands[0])) {
+				break;
+			} else if(!line[0].equals(usableCommands[6])){
+				//int lineNumber = Integer.parseInt(line[1]);
+				if(!symbolTable.containsKey(line[1])) {
+					LineNumber lineNumber = new LineNumber(line[1]);
+					symbolTable.put(lineNumber, commandPointer);
+					commandPointer++;
+					//commandTable.put(lineNumber, line[0]);
+					//newLine.add(lineNumber);
+					objectList[0] = lineNumber;
+					objectList[1] = line[0];
+					if(line.length > 2 && !symbolTable.containsKey(line[2])) {
+						Variable variable = new Variable(line[2]);
+						symbolTable.put(variable, memoryPointer);
+						//newLine.add(variable);
+						objectList[2] = variable;
+					}
+				}
+			}
+			temp.add(objectList);
+		}
+		for(Object[] objectList : temp) {
+			machineCodeContents += commandTable.get(objectList[1]);
+			if(!((String) objectList[1]).equals(usableCommands[6])) {
+				machineCodeContents += symbolTable.get(objectList[2]) + "\n";
+			} else {
+				machineCodeContents += "00";
+			}
+		}
 	}
 }
