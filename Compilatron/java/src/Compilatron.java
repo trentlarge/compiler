36dd2624e7f2e7104fc65bf8792cca71e79fb772
J-Lowe <jonlowe4459@gmail.com>
2013-09-22 21:46:01 -0400

    Added error statements

--- Compilatron/java/src/Compilatron.java
+++ Compilatron/java/src/Compilatron.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JFileChooser;
@@ -31,25 +32,31 @@ public class Compilatron {
 			memory = compiler.compile();
 		}
 		catch(OutOfMemoryException e) {
-			Util.printError("Error: Program requires more memory");
+			Util.printError("Error: Program can only learn 100 moves");
 			System.exit(3);
 		}
 		catch(IllegalArgumentException e) {
+			Util.printError("Error: Lonely if statement at " + compiler.getLineNumber());
 			System.exit(4);
 		}
 		catch(InvalidVariableException e) {
+			Util.printError("Error: Poor naming choice at " + compiler.getLineNumber());
 			System.exit(4);
 		}
 		catch(SyntaxException e) {
+			Util.printError("Error: You dun goofed at " + compiler.getLineNumber());
 			System.exit(4);
 		}
 		catch(GotoException e) {
+			Util.printError("Error: Tried to goto a nonexistent line at " + compiler.getLineNumber());
 			System.exit(5);
 		}
 		catch(LineNumberException e) {
+			Util.printError("Error: Clever, but you can't go back in time at " + compiler.getLineNumber());
 			System.exit(5);
 		}
 		catch(UndefinedVariableException e) {
+			Util.printError("Error: Identity crisis at " + compiler.getLineNumber());
 			System.exit(5);
 		}
 
